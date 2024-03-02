edbe2416e08ebbb76bf252cb1127562788f26de6
RobertFranc <franz4biz@gmail.com>
2024-03-02 16:57:05 +0900

    tmp

--- interface/about_us.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def about_us():
-    st.title("Meet the Team!")
-    st.write("Members profile will be shown in this page with corresponding pictures on the left side")
--- interface/regarding_spotify_interact.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def spotify_page():
-    st.write("Add more information with regards to Spotify interaction of the App")
